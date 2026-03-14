ba5ae5fff47f4df596bd4c9a2fdb3af0b067607f
Tune <dogfriend0311@gmail.com>
2026-03-14 00:03:55 -0700

    fix: ES module export for api/hyperbeam.js

--- api/hyperbeam.js
+++ api/hyperbeam.js
@@ -46,7 +46,7 @@ function teamMatches(espnTeams,targetAbbr){
   return false;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers","Content-Type");
